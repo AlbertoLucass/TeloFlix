4e811581d9143272489a8effc43a9bc38258ca63
Alberto Lucas <38790522+AlbertoLucass@users.noreply.github.com>
2020-07-30 17:40:26 -0300

    Url do Heroku inserido no index

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -29,7 +29,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-      const URL = 'http://localhost:8080/categorias'; 
+      const URL = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://go-flix.herokuapp.com/categorias'; 
       fetch(URL)
        .then(async (respostaDoServer) =>{
           const resposta = await respostaDoServer.json();
